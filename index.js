a32afbc408919ae39b44ebc2b2aea3e79dc45684
Nadim-321 <mdnadimnaiyer@gmail.com>
2022-11-22 23:40:51 +0530

    first

---
+++ index.js
@@ -0,0 +1,265 @@
+// // console.log("hello world");
+// // var myName="nadim naiyer";
+// // console.log(myName);
+// // var myAge=23;
+// // console.log(myAge);
+// // var _my__Name="nadim";
+// // console.log(_my__Name);
+
+
+
+
+// // data types in js
+
+// var myName="Nadim Naiyer";
+// console.log(myName);
+// // type operator
+// console.log(typeof(myName));
+// var myAge=23;
+// console.log(typeof(myAge));
+// var iAMNadim=true;
+// console.log(iAMNadim);
+// console.log(typeof(iAMNadim));
+// console.log(10+ "20");
+// console.log(9- "5");
+// console.log("java" +"script");
+// console.log(" "+ " ")
+// console.log("naiyer"-"nadim");
+// null vs undefined
+// var iAmUseless=null;
+// console.log(iAmUseless);
+
+// var iAmStandBy;
+// console.log(iAmStandBy);
+// NaN===NaN;
+// Number.NaN===NaN;
+// isNaN(NaN);
+// isNaN(Number.NaN);
+// Number.isNaN(Nan);
+
+
+// console.log(isNaN(NaN));
+
+
+
+// console.log(5+20);
+// assignment opretor
+
+// var x=5;
+// var y=5;
+// console.log("is a both the x and y are equal or not " + x==y);
+// // arthemetic opretors
+// console.log(1+2);
+// console.log(10-5);
+// console.log(20/5);
+// console.log(3*5);
+
+
+
+
+
+// incriment or decriment opretors
+
+// postfix opretions
+
+// var num=15;
+// var newNUm=num++;
+// console.log(num);
+// console.log(newNUm);
+
+// prefix opretions
+
+// var num=15;
+// var newNum=++num +5;
+// console.log(num);
+// console.log(newNum);
+
+
+// comparison opretor
+
+// var a=30;
+// var b=10;
+// console.log(a==b);
+// console.log(a !=b);
+// console.log(a>b)
+
+// logical opretosr
+
+var a=30;
+var b=-20;
+// logical AND opretor
+
+// console.log(a>b && b>0);
+
+// logical or opretor
+// console.log(a>b || b>0);
+
+// string opretors
+// console.log("hello world");
+// console.log("hello" + "world")
+
+// challenge time 4
+// console.log(3**3);
+// console.log(3**4);
+// ?shop 2 numbers
+// var a=5;
+// var b=10;
+// var c=b;
+// b=a;
+// a=c;
+// console.log("the value of a is " +a);
+// console.log("the value of a is " +b);
+
+
+// loops in js
+
+// if raining =raincoat;
+// else no raincoat;
+
+
+// var tomr="rain";
+// var tomr="sunny";
+
+
+// if(tomr=="rain"){
+//     console.log("take a raincoat");
+
+// }else{
+//     console.log("no need to raincoat")
+// }
+
+
+// leap year
+
+
+// var year=2020;
+
+
+// if(year %4===0){
+//     if(year%100===0){
+//         if(year%400===0){
+//             console.log("the year"+ year + " is a leaper year");
+//         }else{
+//             console.log("the year"+ yera+ " in not leaper year");
+//         }
+
+//     }else{
+//         console.log("the year"+ year+ " in not leaper year");
+
+//     }
+
+// }else{
+//     console.log("the year" + year+ "is a leaper year");
+
+
+// }
+
+
+
+
+// truthy and falsey (0,false,undefined,NaN,null)
+
+// if(score =0){
+//     console.log("yeh,we won the game");
+
+
+// }else{
+//     console.log("omg ,we loss the game");
+// }
+
+
+
+// conditional(ternary) opretors
+
+// the conditional (ternary) operator is the only javascript opretor that takes three operends
+// var age=18;
+// if(age>=18){
+//     console.log("you are eligible to votre");
+// }else{
+//     console.log("you are  not eligible to votre");
+// }
+
+// var age=20;
+// console.log( (age>=18) ? "you can vote" : "you can't vote");
+
+
+// switch statement
+// evaluatesan expression ,matching the expression's value ton a case clause ,
+// and execute statements associated with taht case .
+
+// 1st with out break statement
+// find the area of tringle,circle,rectangl?
+
+// var area="square";
+// var PI=3.142,l=5, b=4, r=3;
+// if(area=="circle"){
+//     console.log(" the area of circle is:"= +PI*r**2);
+
+// }else if(area=="tringle"){
+//     console.log("the area of tringle:" +(l*b)/2);
+
+// }else if(area=="rectangle"){
+
+
+// console.log("the are of rectangle:" + (l*b));
+// }else{
+//     console.log("plese inter valid data");
+// }
+
+// var area="circle";
+// var PI=3.142,l=5, b=4, r=3;
+// switch(area){
+//     case "circle":
+//     console.log(" the area of circle is:"= +PI*r**2);
+//     break;
+
+//     case "triangle":
+//         console.log("the area of tringle:" +(l*b)/2);    
+//         break;
+
+// case "rectanglr":
+//     console.log("the are of rectangle:" + (l*b));
+//     break;
+//     default:
+//         console.log("plese inter valid data");
+
+// }
+
+// while loop
+// the while staement creates a loop that execute a specified statement
+// as long as the test condition evalutes to true.
+// var num=0;
+// while(num<=10){
+//     console.log(num++);
+// }
+
+
+// do while loop
+
+// var num =0;
+
+// do{
+//     console.log(num);
+//     num++;
+
+// }while(num<=10);
+
+
+
+// for loop
+
+// for( var num=0;num<=10;num++){
+// console.log(num);
+// }\
+
+
+// javascript program to print table for given number 
+// (8,9,12,15) using for loop?
+
+
+
+
+
+
+
+ 
